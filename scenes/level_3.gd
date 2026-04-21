d54b2fdc985b16a50ddcaa62d736ec67b6296f60
Davi-Fiori <davireveral007@gmail.com>
2026-04-21 08:29:00 -0300

    Alteração Lógica fase 3

--- scenes/level_3.gd
+++ scenes/level_3.gd
@@ -2,36 +2,38 @@ extends Node2D
 
 @onready var key = $chave
 @onready var door = $Tiles/porta
+
+# ATENÇÃO: Se o erro "null instance" voltar, é este caminho abaixo que você precisa
+# arrumar segurando Ctrl e arrastando o caranguejo da aba Scene para cá!
+@onready var player = $Tiles/caranguejo
+
 @onready var timer = $Timer
 @onready var question_mark_label = $HUD/HBoxContainer/TextureRect3/interrogacao
 @onready var time_label = $HUD/TimeLabel
 
 var correct_answer = 3
+var can_enter_door = false
 
 func _ready():
-	# Hide and disable the key and door at the start
 	key.hide()
 	key.process_mode = Node.PROCESS_MODE_DISABLED
 	door.hide()
 	door.process_mode = Node.PROCESS_MODE_DISABLED
 	
-	# Connect the timer
 	timer.timeout.connect(_on_timer_timeout)
+	door.body_entered.connect(_on_door_body_entered)
 
 func _on_timer_timeout():
 	restart_level()
 
 func check_answer(picked_value):
 	if picked_value == correct_answer:
-		# Change the "?" to the correct answer (converts the number to text)
 		question_mark_label.text = str(correct_answer)
-		
-		# Delete the floating numbers and spawn the key
 		get_tree().call_group("numbers", "queue_free")
 		spawn_key()
 	else:
-		# Wrong answer
-		get_tree().call_deferred("reload_current_scene")
+		# Usa a variável 'player' que criamos lá em cima
+		player.die()
 
 func spawn_key():
 	key.show()
@@ -41,11 +43,32 @@ func show_door():
 	door.show()
 	door.process_mode = Node.PROCESS_MODE_INHERIT
 	door.get_node("AnimatedSprite2D").play("abrindo")
+	
+	await get_tree().create_timer(1.0).timeout
+	
+	can_enter_door = true
+	
+	for body in door.get_overlapping_bodies():
+		if body.name == "caranguejo":
+			go_to_next_level()
+
+func _on_door_body_entered(body):
+	if body.name == "caranguejo" and can_enter_door == true:
+		go_to_next_level()
+
+func go_to_next_level():
+	var current_path = get_tree().current_scene.scene_file_path 
+	var level_number = current_path.get_file().get_basename().trim_prefix("level_").to_int()
+	var next_level_number = level_number + 1
+	var next_level_path = "res://scenes/level_" + str(next_level_number) + ".tscn"
+	
+	if ResourceLoader.exists(next_level_path):
+		get_tree().change_scene_to_file(next_level_path)
+	else:
+		print("VOCÊ ZEROU O JOGO!")
 
 func restart_level():
 	get_tree().call_deferred("reload_current_scene")
 	
 func _process(delta):
-	# We use int() to chop off the decimals, otherwise it shows as 29.847291
-	# Then we use str() to turn that number into text for the Label
 	time_label.text = str(int(timer.time_left))
