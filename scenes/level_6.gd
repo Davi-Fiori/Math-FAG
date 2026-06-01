bda6d7b5ac03134a8b7db9dddf3d4513d8c4dadc
Davi-Fiori <davireveral007@gmail.com>
2026-06-01 19:36:34 -0300

    Fase 7-8

---
+++ scenes/level_6.gd
@@ -0,0 +1,77 @@
+extends Node2D
+
+@onready var question_mark_1 = $"HUD/HBoxContainer/TextureRect/numero 1"
+@onready var question_mark_2 = $HUD/HBoxContainer/TextureRect3/interrogacao
+@onready var key = $chave
+@onready var door = $porta
+@onready var timer = $Timer
+@onready var time_label = $HUD/TimeLabel
+@onready var player = $caranguejo
+
+var can_enter_door = false
+var required_numbers = [6,9]
+var collected_numbers = []
+
+func _ready():
+	key.hide()
+	key.process_mode = Node.PROCESS_MODE_DISABLED
+	door.hide()
+	door.process_mode = Node.PROCESS_MODE_DISABLED
+	
+	timer.timeout.connect(_on_timer_timeout)
+	# The Level is now the ONLY script listening for the door touch!
+	door.body_entered.connect(_on_door_body_entered)
+
+func _on_timer_timeout():
+	restart_level()
+
+func check_answer(picked_value):
+	# 1. Check if the grabbed number is one of the correct ones
+	if picked_value in required_numbers and not picked_value in collected_numbers:
+		
+		# Add it to our collected list
+		collected_numbers.append(picked_value)
+		
+		# 2. Update the HUD from left to right
+		if collected_numbers.size() == 1:
+			question_mark_1.text = str(picked_value)
+		elif collected_numbers.size() == 2:
+			question_mark_2.text = str(picked_value)
+			
+		# 3. Check if we have collected ALL the required numbers
+		if collected_numbers.size() == required_numbers.size():
+			# (We removed timer.stop() from here so the clock keeps ticking!)
+			get_tree().call_group("numbers", "queue_free")
+			spawn_key()
+			
+	else:
+		# WRONG ANSWER
+		$caranguejo.die()
+
+func spawn_key():
+	key.show()
+	key.process_mode = Node.PROCESS_MODE_INHERIT
+
+func show_door():
+	door.show()
+	door.process_mode = Node.PROCESS_MODE_INHERIT
+	door.get_node("AnimatedSprite2D").play("abrindo")
+	
+	await get_tree().create_timer(1.0).timeout
+	can_enter_door = true
+	
+	for body in door.get_overlapping_bodies():
+		if body.name == "caranguejo":
+			# WE NOW CALL THE DOOR SCRIPT AND SEND IT THE TIME!
+			door.advance_level(timer.time_left)
+
+func _on_door_body_entered(body):
+	if body.name == "caranguejo" and can_enter_door == true:
+		# WE NOW CALL THE DOOR SCRIPT AND SEND IT THE TIME!
+		door.advance_level(timer.time_left)
+
+func restart_level():
+	get_tree().call_deferred("reload_current_scene")
+
+func _process(_delta):
+	time_label.text = str(int(timer.time_left))
---
+++ scenes/level_7.gd
@@ -0,0 +1,80 @@
+extends Node2D
+
+# Caminhos ajustados olhando exatamente a sua imagem!
+@onready var question_mark_1 = $"HUD/HBoxContainer/TextureRect3/interrogacao"
+@onready var question_mark_2 = $"HUD/HBoxContainer2/TextureRect3/interrogacao"
+
+@onready var key = $chave
+@onready var door = $porta
+@onready var timer = $Timer
+@onready var time_label = $HUD/TimeLabel
+@onready var player = $caranguejo
+
+var can_enter_door = false
+# Lista com as respostas corretas das duas expressões
+var required_numbers = [8, 9]
+var collected_numbers = []
+
+func _ready():
+	key.hide()
+	key.process_mode = Node.PROCESS_MODE_DISABLED
+	door.hide()
+	door.process_mode = Node.PROCESS_MODE_DISABLED
+	
+	timer.timeout.connect(_on_timer_timeout)
+	door.body_entered.connect(_on_door_body_entered)
+
+func _on_timer_timeout():
+	restart_level()
+
+func check_answer(picked_value):
+	# 1. Verifica se o número coletado faz parte das respostas e se ainda não foi pego
+	if picked_value in required_numbers and not picked_value in collected_numbers:
+		
+		# Adiciona à lista de coletados para sabermos o progresso
+		collected_numbers.append(picked_value)
+		
+		# 2. ENCAIXA NO LUGAR CERTO:
+		if picked_value == 8:
+			question_mark_1.text = str(picked_value) # Vai para o HBoxContainer (7 + 8 = 15)
+		elif picked_value == 9:
+			question_mark_2.text = str(picked_value) # Vai para o HBoxContainer2 (9 + 9 = 18)
+			
+		# 3. Verifica se o jogador resolveu todas as expressões do nível
+		if collected_numbers.size() == required_numbers.size():
+			get_tree().call_group("numbers", "queue_free")
+			spawn_key()
+			
+	else:
+		# Se o jogador pegar um número errado (como o 3), ele perde
+		$caranguejo.die()
+
+func spawn_key():
+	key.show()
+	key.process_mode = Node.PROCESS_MODE_INHERIT
+
+func show_door():
+	door.show()
+	door.process_mode = Node.PROCESS_MODE_INHERIT
+	door.get_node("AnimatedSprite2D").play("abrindo")
+	
+	await get_tree().create_timer(1.0).timeout
+	can_enter_door = true
+	
+	for body in door.get_overlapping_bodies():
+		if body.name == "caranguejo":
+			door.advance_level(timer.time_left)
+
+func _on_door_body_entered(body):
+	if body.name == "caranguejo" and can_enter_door == true:
+		door.advance_level(timer.time_left)
+
+func restart_level():
+	get_tree().call_deferred("reload_current_scene")
+
+func _process(_delta):
+	time_label.text = str(int(timer.time_left))
+
+
+func _on_num_6_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
